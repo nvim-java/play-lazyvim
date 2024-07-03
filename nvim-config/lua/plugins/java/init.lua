5736285957f695a9d56fc5431c6b711963c93d95
s1n7ax <srineshnisala@gmail.com>
2024-07-03 07:33:53 +0530

    refactor: using the new config

--- nvim-config/lua/plugins/java/init.lua
+++ nvim-config/lua/plugins/java/init.lua
@@ -1,33 +1,23 @@
 return {
   "nvim-java/nvim-java",
+  config = false,
   dependencies = {
-    "nvim-java/lua-async-await",
-    "nvim-java/nvim-java-refactor",
-    "nvim-java/nvim-java-core",
-    "nvim-java/nvim-java-test",
-    "nvim-java/nvim-java-dap",
-    "MunifTanjim/nui.nvim",
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
     {
-      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
       opts = {
-        registries = {
-          "github:nvim-java/mason-registry",
-          "github:mason-org/mason-registry",
+        servers = {
+          jdtls = {
+            -- Your custom jdtls settings goes here
+          },
         },
-      },
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        handlers = {
-          ["jdtls"] = function()
-            require("java").setup()
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              -- Your custom nvim-java configuration goes here
+            })
           end,
         },
       },
     },
   },
-  opts = {},
 }

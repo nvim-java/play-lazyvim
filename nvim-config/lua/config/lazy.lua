55525627236008d9c71609250061e6ce44f9dde5
s1n7ax <srineshnisala@gmail.com>
2024-10-21 18:29:26 +0530

    feat: add debug core extras

--- nvim-config/lua/config/lazy.lua
+++ nvim-config/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.dap.core" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
